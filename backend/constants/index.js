2161332e48b5c2b021cc1ca3b741737008b755a9
Akash Jyoti Sahoo <akashvana2001@gmail.com>
2023-06-04 23:54:05 +0530

    first commit

---
+++ backend/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xEF3Ba5Fc191B1148B8a46f660838851649996FC0";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
